c1fd6b483fe6081fa05299e002dda500830b0cfb
Yazima <yazima04200@gmail.com>
2022-10-24 23:21:43 +0900

    edit機能追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,26 @@ class ArticlesController < ApplicationController
     @articles = Article.find(params[:id])
   end
 
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @articles = Article.new(articles.params)
+    if @articles.save
+      redirect_to articles_path
+    else
+      render 'new'
+    
+   end
+  end
+
+  def edit
+    @articles = Article.find(params[:id])
+  
+  private
+    def articles_params
+      params.require(:articles).permit(:title, :body)
+    end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence :true, length: { minimum:3, message: '短すぎて投稿できません！' }
+  validates :body, presence :true
 end
